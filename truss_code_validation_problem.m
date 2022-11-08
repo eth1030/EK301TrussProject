36060a30040fc80b74f58034a107224cb80e3b81
Christian <christian@crc-dot1x-nat-10-239-234-98.bu.edu>
2022-11-08 15:01:34 -0500

    updated

--- truss_code_validation_problem.m
+++ truss_code_validation_problem.m
@@ -8,10 +8,16 @@ input_c = [
     1 2, %member 1
     1 3, %member 2
     2 3, %member 3
-    3 4, %member 4
-    2 4, %member 5
-    2 5, %member 6
-    4 5  %member 7
+    2 4, %member 4
+    3 4, %member 5
+    4 5, %member 6
+    3 5, %member 7
+    3 6, %member 8
+    5 6, %member 9
+    5 7, %member 10
+    6 7, %member 11
+    7 8, %member 12
+    6 8, %member 13
     ];
 
 C = zeros(j, m);
@@ -22,12 +28,29 @@ for r = 1:size(input_c,1)
 
 end
 
-Sy = zeros(j, 3); %matrix along the y-axis
 Sx = zeros(j, 3); %matrix along the x-axis
+Sy = zeros(j, 3); %matrix along the y-axis
 
-X = 
-%Y = 
+%reaction forces
+x1 = 1; %x1 reaction at joint 1
+y1 = 1; %y1 reaction at joint 1
+y2 = 8; %y2 reaction at joint 8
 
-m = 1.7; %1.7lb
-w = m*(32.2); %finding weight force
-L = [0, 0, 0, 0, 0, 0, w, 0, 0, 0].'
+Sx(x1, 1) = 1;
+Sy(y1, 2) = 1;
+Sy(y2, 3) = 1;
+
+%length of members in vectors
+r = zeros(1,m)
+for i = 1:m
+    r(i) = sqrt((X(input_c(i,2))-X(input_c(i,1)))^2 + (Y(input_c(i,2))-Y(input_c(i,1)))^2)
+end
+
+X = [0 0 4 4 8 8 12 12]; %in meters
+Y = [0 4 4 8 8 4 4 0]; %in meters 
+
+
+w = 25; %weight force 25N
+L = zeros(j, 1)
+L(3) = w;  %weight force on joint 3
+
