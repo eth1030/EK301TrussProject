0857b59b41a1b805fd245e58d0aace5c6fe4f705
Emika Hammond <98787002+eth1030@users.noreply.github.com>
2022-11-11 03:09:00 -0500

    Update maxload.m

--- maxload.m
+++ maxload.m
@@ -10,23 +10,34 @@ Rm = T(1:size(T,1)-3) ./ Wl;
 length = r(1:size(T,1)-3);
 
 Pcrit = 2945 ./ (length.^2);
-%disp("Pcrit: ")
-%disp(Pcrit');
+disp("Pcrit: ")
+disp(Pcrit');
 
 %% calculate Wfailure
 Pcrit = Pcrit';
 %Wfailure = -Pcrit./ Rm;
 Wfailure = rdivide(-Pcrit,Rm);
-%disp("Wfailure");
-%disp(Wfailure);
+disp("Wfailure");
+disp(Wfailure);
 
 %% find minimum Wfailure
 Wfailure(Wfailure==-Inf) = max(Wfailure);
 %disp(Wfailure);
-Wfailure = abs(Wfailure);
+Wfailure(Wfailure<0) = max(Wfailure);
 buckle = min(Wfailure);
 fprintf("max load : %f\n", buckle);
 B = Wfailure == buckle;
 %disp(B);
 indice = find(B);
 fprintf("member: %d\n",indice);
+%{
+%%print to file
+fileID = fopen('designs.txt','a');
+fprintf(fileID, "Design #\n");
+fprintf(fileID,"Cost of truss: $"+cost+"\n");
+fprintf(fileID,"Theoretical max load/cost ratio in oz/$: "+ sum(L)/cost+"\n");
+fprintf(fileID,"max load : %f\n", buckle);
+fprintf(fileID,"member: %d\n",indice);
+fprintf(fileID," \n");
+fclose(fileID);
+%}
